82cf0a3d25e3c056699b8e276959409724a2448a
easgarli <e.asgarli@gmail.com>
2025-04-29 18:09:15 +0400

    changes from local comp

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('loans/', include('banking_aggregator.apps.loans.urls')),
     path('banks/', include('banking_aggregator.apps.banks.urls')),
     path('users/', include('banking_aggregator.apps.users.urls')),
+    path('users/', include('django.contrib.auth.urls')),  # Include built-in auth URLs
     path('', include('banking_aggregator.apps.core.urls')),  # Use core app for the root path
 ]
